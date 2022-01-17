c7a088fd09b31ad73280031e9416750f22d3c3b3
Shaps7777 <shaps@list.ru>
2022-01-17 21:00:21 +0300

    Оптимизация работы со списками повремени

--- hw_5_2.py
+++ hw_5_2.py
@@ -13,22 +13,33 @@ print(new_list)
 Использовать компрехеншн.
 """
 from random import randint
+from time import perf_counter
+
 # Вариант 1. Пробегаем посписку и записываем только те значения
 # которые встречаются 1 раз (my_list.count(el) == 1)
-my_list = [randint(-10, 10) for _ in range(20)]
+my_list = [randint(-10, 10) for _ in range(20000)]
+start = perf_counter()
 uniq_list = [el for el in my_list if my_list.count(el) == 1]
 print(f'Начальный список: {my_list}\nСписок без повтора: {uniq_list}')
+print(f'Время выполнения операции: {perf_counter() - start}')
 
 # Вариант 2. Делаем через множества.Вначале цикл: если элемента в check нет -
 # записываем туда и в uniq. Как только значение 2-й раз встречается,
 # перескакиваем в else и изымаем из uniq записанное туда ранее повторившееся значение
+start = perf_counter()
 check = set()
 uniq = set()
+# Если делать упорядоченный список, то
+# uniq = []
 for el in my_list:
     if el not in check:
         check.add(el)
         uniq.add(el)
+        # И тогда
+        # uniq.append(el)
     else:
         if el in uniq:
             uniq.remove(el)
+
 print(f'Список без повтора: {uniq}')
+print(f'Время выполнения операции: {perf_counter() - start}')
